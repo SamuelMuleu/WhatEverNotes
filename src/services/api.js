49985fbeb4cf55d8afa0b33d5995739d59e5317c
Samuel Pereira Muleu <samuelpereiramuleu@gmail.com>
2023-12-10 16:31:19 -0300

    backend upgrade

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://whatevernotes-api.onrender.com"
+    baseURL: "https://whatevernotes-backend.onrender.com"
 });
 
